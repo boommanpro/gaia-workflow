40d98b6e8e87954d7dabf247636751168f53cccd
boommanpro <boommanpro@gmail.com>
2025-09-07 22:48:51 +0800

    feat(补充main方法):

---
+++ gaia-workflow-server/src/main/java/cn/boommanpro/gaiaworkflow/GaiaWorkflowServerDemoApplication.java
@@ -0,0 +1,18 @@
+package cn.boommanpro.gaiaworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Gaia工作流服务启动类
+ *
+ * @author <a href="mailto:boommanpro@gmail.com">boommanpro</a>
+ */
+@SpringBootApplication
+public class GaiaWorkflowServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaiaWorkflowServerDemoApplication.class, args);
+    }
+
+}
